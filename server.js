ab24fb4cdb65aa33600de907841e263a31e3a5d1
Oluwadaprof <adeekotobiloba8@gmail.com>
2025-02-05 00:28:57 +0100

    feat: 🎸 express crash course setup

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+// setup static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+  { id: 1, title: "Post One" },
+  { id: 2, title: "Post Two" },
+  { id: 3, title: "Post Three" },
+];
+
+// Get all posts
+app.get("/api/posts", (req, res) => {
+  const limit = parseInt(req.query.limit);
+  if (!isNaN(limit) && limit > 0) {
+    return res.status(200).json(posts.slice(0, limit));
+  }
+  res.status(200).json(posts);
+});
+
+// Get single post
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (!post) {
+   return res.status(400).json({ message: `Post with the id of ${id} not found` });
+  } 
+    res.status(200).json(post);
+  
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
